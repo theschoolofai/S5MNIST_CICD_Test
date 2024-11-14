e115fbf01f158e1843cb8ffa61242ab327031d1c
admin@theschoolofai.in <admin@theschoolofai.in>
2024-11-14 08:56:33 +0530

    model under 100k

--- model/train.py
+++ model/train.py
@@ -8,9 +8,9 @@ import os
 class SimpleCNN(nn.Module):
     def __init__(self):
         super(SimpleCNN, self).__init__()
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
-        self.fc1 = nn.Linear(32 * 7 * 7, 128)
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(10, 10, kernel_size=3, padding=1)
+        self.fc1 = nn.Linear(10 * 7 * 7, 128)
         self.fc2 = nn.Linear(128, 10)
         self.pool = nn.MaxPool2d(2, 2)
         self.relu = nn.ReLU()
@@ -18,7 +18,7 @@ class SimpleCNN(nn.Module):
     def forward(self, x):
         x = self.pool(self.relu(self.conv1(x)))
         x = self.pool(self.relu(self.conv2(x)))
-        x = x.view(-1, 32 * 7 * 7)
+        x = x.view(-1, 10 * 7 * 7)
         x = self.relu(self.fc1(x))
         x = self.fc2(x)
         return x
